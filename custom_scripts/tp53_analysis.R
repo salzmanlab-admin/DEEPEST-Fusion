6aeabade92660d33c480b97bd66aea7a7d2dc7ea
Salzman Lab <salzmanlab@stanford.edu>
2019-05-16 13:52:49 -0700

    Update tp53_analysis.R

--- custom_scripts/tp53_analysis.R
+++ custom_scripts/tp53_analysis.R
@@ -5,56 +5,43 @@ library(dplyr)
 library(ggplot2)
 
 
-################### Inputs #####################
-cell_fusions_knife_file = "/scratch/PI/horence/Roozbeh/TCGA_Project/pancancer_fusion_lists/cell_fusions_knife.csv"
-cell_fusions_smachete_file = "/scratch/PI/horence/Roozbeh/TCGA_Project/pancancer_fusion_lists/cell_fusions_smachete.csv"
-P53_freq_file = "/scratch/PI/horence/Roozbeh/TCGA_Project/systems_biology_files/TP53_mutation.txt"
-average_num_smachete_fusions_file = "/scratch/PI/horence/Roozbeh/TCGA_Project/consolidated_files/mean_detected_fusion_per_cancer.txt"
-cell_paper_samples_file = "/scratch/PI/horence/Roozbeh/TCGA_Project/utility_files/samples_used_in_cell_paper.csv"
-##################################################
+#All input files are available in DEEPEST-Fusion Github repository, custom scripts folder at: https://github.com/salzmanlab/DEEPEST-Fusion/tree/master/custom_scripts/files 
 
-
-################### Inputs (local PC) #####################
-cell_fusions_knife_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\pancancer_fusion_lists\\cell_fusions_knife.csv"
-cell_fusions_smachete_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\pancancer_fusion_lists\\cell_fusions_smachete.csv"
-P53_freq_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\systems_biology_files\\TP53_mutation.txt"
-average_num_smachete_fusions_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\consolidated_files\\mean_detected_fusion_per_cancer.txt"
-cell_paper_samples_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\utility_files\\samples_used_in_cell_paper.csv"
-TCGA_fusions_with_id_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\consolidated_files\\TCGA_fusions_smachete_knife_with_ID.txt"
-smachete_samples_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\utility_files\\samples_used_in_smachete.txt"
-P53_mutated_samples_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\systems_biology_files\\TCGA_samples_with_tp53_mutation.tsv"
-tumorfusions_smachete_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\pancancer_fusion_lists\\tumorfusions_smachete.csv"
-tumorfusions_knife_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\pancancer_fusion_lists\\tumorfusions_knife.csv"
-tumorfusions_samples_file = "G:\\My Drive\\Postdoc_Research\\Projects\\TCGA (sMACHETE)\\utility_files\\samples_used_in_PRADA.csv"
+################### Inputs files #####################
+cell_fusions_file = "DEEPEST-Fusion/custom_scripts/files/cell_fusions.csv"
+P53_freq_file = "DEEPEST-Fusion/custom_scripts/files/TP53_mutation.txt"
+average_num_deepest_fusions_file = "DEEPEST-Fusion/custom_scripts/files/mean_detected_fusion_per_cancer.txt"
+cell_paper_samples_file = "DEEPEST-Fusion/custom_scripts/files/samples_used_in_cell_paper.csv"
+DEEPEST_fusions_with_ID_file = "DEEPEST-Fusion/custom_scripts/files/DEEPEST_fusions_with_ID.txt"
+DEEPEST_samples_file = "DEEPEST-Fusion/custom_scripts/files/samples_used_in_DEEPEST.txt"
+P53_mutated_samples_file = "DEEPEST-Fusion/custom_scripts/files/TCGA_samples_with_tp53_mutation.tsv"
+tumorfusions_file = "DEEPEST-Fusion/custom_scripts/files/tumorfusions.csv"
+tumorfusions_samples_file = "DEEPEST-Fusion/custom_scripts/files/samples_used_in_PRADA.csv"
 ###########################################################
 
 
 
 #######  read in input files  #########################
-cell_fusions_knife = fread(cell_fusions_knife_file,sep=",",header = TRUE)
-cell_fusions_smachete = fread(cell_fusions_smachete_file,sep=",",header = TRUE)
+cell_fusions = fread(cell_fusions_knife_file,sep=",",header = TRUE)
 samples_used_in_cell_paper = fread(cell_paper_samples_file, header = TRUE, sep = ",")
 P53_freq = fread(P53_freq_file,header = TRUE,sep="\t")
-average_num_smachete_fusions = fread(average_num_smachete_fusions_file,header = TRUE,sep="\t")
+average_num_DEEPEST_fusions = fread(average_num_DEEPEST_fusions_file,header = TRUE,sep="\t")
 samples_used_in_cell_paper = fread(cell_paper_samples_file, header = TRUE, sep = ",")
-TCGA_fusions_with_id = fread(TCGA_fusions_with_id_file,sep="\t",header = TRUE)
-samples_used_in_smachete = fread(smachete_samples_file,header = TRUE,sep = "\t" )
-TCGA_fusions_with_id = fread(TCGA_fusions_with_id_file,sep="\t",header = TRUE)
+samples_used_in_DEEPEST = fread(DEEPEST_samples_file,header = TRUE,sep = "\t" )
+DEEPEST_fusions_with_ID = fread(DEEPEST_fusions_with_ID_file,sep="\t",header = TRUE)
 P53_mutated_samples = fread(P53_mutated_samples_file,sep="\t",header=TRUE)
-tumorfusions_knife = fread(tumorfusions_knife_file, header = TRUE, sep = ",") 
-tumorfusions_smachete = fread(tumorfusions_smachete_file, header = TRUE, sep = ",")
+tumorfusions = fread(tumorfusions_knife_file, header = TRUE, sep = ",") 
 samples_used_in_tumorfusions = fread(tumorfusions_samples_file, header = TRUE, sep = ",")
 ##############################################
 
 
-###compute correlations for smachete
-average_num_smachete_fusions_vs_p53_freq = merge(average_num_smachete_fusions,P53_freq,by.x = "TCGA_Project",by.y = "cancer")
-smachete_pearson = cor(average_num_smachete_fusions_vs_p53_freq$mean, average_num_smachete_fusions_vs_p53_freq$mutation_frequency, method = "pearson")
-smachete_spearman = cor(average_num_smachete_fusions_vs_p53_freq$mean, average_num_smachete_fusions_vs_p53_freq$mutation_frequency, method = "spearman")
+###compute correlations for DEEPEST
+average_num_DEEPEST_fusions_vs_p53_freq = merge(average_num_DEEPEST_fusions,P53_freq,by.x = "TCGA_Project",by.y = "cancer")
+DEEPEST_pearson = cor(average_num_DEEPEST_fusions_vs_p53_freq$mean, average_num_DEEPEST_fusions_vs_p53_freq$mutation_frequency, method = "pearson")
+DEEPEST_spearman = cor(average_num_DEEPEST_fusions_vs_p53_freq$mean, average_num_DEEPEST_fusions_vs_p53_freq$mutation_frequency, method = "spearman")
 
 
 #####compute correlations for cell
-cell_fusions = rbind(cell_fusions_knife,cell_fusions_smachete)
 tumor_samples_used_in_cell_paper = samples_used_in_cell_paper[!((Sample%like%"-11A-") | (Sample%like%"-11B-") | (Sample%like%"-11C-"))]
 num_cell_samples = tumor_samples_used_in_cell_paper[,length(Sample),by = Cancer]
 num_tot_cell_fusions_per_cancer = cell_fusions[,length(Fusion),by=Cancer]
@@ -67,7 +54,6 @@ cell_spearman = cor(average_num_cell_fusions_vs_p53_freq$V1,  average_num_cell_f
 
 
 #####compute correlations for prada
-tumorfusions = rbind(tumorfusions_knife,tumorfusions_smachete)
 tumor_samples_used_in_tumorfusions = samples_used_in_tumorfusions[!((barcode%like%"-11A-") | (barcode%like%"-11B-") | (barcode%like%"-11C-"))]
 num_tumorfusions_samples = tumor_samples_used_in_tumorfusions[,.N,by = Disease]
 num_tot_tumorfusions_per_cancer = tumorfusions[,.N,by=Cancer]
@@ -82,15 +68,15 @@ tumorfusions_spearman = cor(average_num_tumorfusions_vs_p53_freq$V1,  average_nu
 #comarping the abudnance of fusions in samples w and w/o TP53  mutation
 
 
-samples_used_in_smachete[,sample_name_new:=substr(sample_name,1,nchar(sample_name)-1),by=1:nrow(samples_used_in_smachete)]
-samples_used_in_smachete[,is.TP53_mutated:=0]
-samples_used_in_smachete[sample_name_new%in%P53_mutated_samples$`Sample ID`,is.TP53_mutated:=1]
-samples_used_in_smachete = samples_used_in_smachete[!((sample_name%like%"11A") | (sample_name%like%"11B") | (sample_name%like%"11C"))]
+samples_used_in_DEEPEST[,sample_name_new:=substr(sample_name,1,nchar(sample_name)-1),by=1:nrow(samples_used_in_DEEPEST)]
+samples_used_in_DEEPEST[,is.TP53_mutated:=0]
+samples_used_in_DEEPEST[sample_name_new%in%P53_mutated_samples$`Sample ID`,is.TP53_mutated:=1]
+samples_used_in_DEEPEST = samples_used_in_DEEPEST[!((sample_name%like%"11A") | (sample_name%like%"11B") | (sample_name%like%"11C"))]
 
-TCGA_fusions_with_id[,sample_fusion:=paste(sample_name,fusion_unified,sep="--"),by=1:nrow(TCGA_fusions_with_id)]
-TCGA_fusions_with_id = TCGA_fusions_with_id[!(duplicated(sample_fusion))]
-num_fusion_per_sample = TCGA_fusions_with_id[,.N,by=sample_name]
-samples_num_fusion_tp53 = merge(samples_used_in_smachete,num_fusion_per_sample,by.x = "sample_name",by.y = "sample_name",all.x=TRUE,all.y = FALSE)
+DEEPEST_fusions_with_ID[,sample_fusion:=paste(sample_name,fusion_unified,sep="--"),by=1:nrow(DEEPEST_fusions_with_ID)]
+DEEPEST_fusions_with_ID = DEEPEST_fusions_with_ID[!(duplicated(sample_fusion))]
+num_fusion_per_sample = DEEPEST_fusions_with_ID[,.N,by=sample_name]
+samples_num_fusion_tp53 = merge(samples_used_in_DEEPEST,num_fusion_per_sample,by.x = "sample_name",by.y = "sample_name",all.x=TRUE,all.y = FALSE)
 samples_num_fusion_tp53[is.na(N)]$N=0
 
 samples_num_fusion_tp53$is.TP53_mutated=as.factor(samples_num_fusion_tp53$is.TP53_mutated)
@@ -113,13 +99,13 @@ for (counter in 1:25){
 #  print(g +  geom_violin(width = 0.5,lwd =0.7,aes(fill=factor(is.TP53_mutated)))+coord_cartesian(ylim = c(0 ,max(samples_num_fusion_tp53_per_cancer[is.TP53_mutated=="Yes"]$N,samples_num_fusion_tp53_per_cancer[is.TP53_mutated=="No"]$N)*1.01))+ labs(title=TCGA_Cancers[counter])+ stat_summary(fun.y=mean, geom="point", shape=20, size=5, color="black", fill="black")+xlab("TP53 mutation") + ylab("Number of fusions")+theme(legend.position="none",axis.text.x = element_text(size=14),axis.text.y = element_text(size=14),axis.title.x = element_text(size=16),axis.title.y = element_text(size=16)))
 }
 
-####plot the graph for average fusion by smachete vs TP53 mutation frequency
-plot(average_num_smachete_fusions_vs_p53_freq$mutation_frequency, average_num_smachete_fusions_vs_p53_freq$mean, xlim=c(0,95),ylim=c(0,9.5), xlab="TP53 mutation frequency (%)" ,ylab="Average number of fusions", pch=19,cex=1.4,cex.lab=1.4)
+####plot the graph for average fusion by DEEPEST vs TP53 mutation frequency
+plot(average_num_DEEPEST_fusions_vs_p53_freq$mutation_frequency, average_num_DEEPEST_fusions_vs_p53_freq$mean, xlim=c(0,95),ylim=c(0,9.5), xlab="TP53 mutation frequency (%)" ,ylab="Average number of fusions", pch=19,cex=1.4,cex.lab=1.4)
 text(13,9, "Pearson's r = 0.49667", col = 1,cex=1.2)
 text(13,8, "Spearman's rho = 0.6370", col = 1,cex=1.2)
-abline(lm(average_num_smachete_fusions_vs_p53_freq$mean ~ average_num_smachete_fusions_vs_p53_freq$mutation_frequency),col=1,cex=20)
+abline(lm(average_num_DEEPEST_fusions_vs_p53_freq$mean ~ average_num_DEEPEST_fusions_vs_p53_freq$mutation_frequency),col=1,cex=20)
 
-x_pos= average_num_smachete_fusions_vs_p53_freq$mutation_frequency-2
+x_pos= average_num_DEEPEST_fusions_vs_p53_freq$mutation_frequency-2
 x_pos[17]=x_pos[17]+8
 x_pos[9]=x_pos[9]+8
 x_pos[24]=x_pos[24]+8
@@ -131,9 +117,9 @@ x_pos[14]=x_pos[14]+4
 x_pos[12]=x_pos[12]+7
 x_pos[4]=x_pos[4]+3
 x_pos[21]=x_pos[21]+1
-y_pos=average_num_smachete_fusions_vs_p53_freq$mean+0.5
+y_pos=average_num_DEEPEST_fusions_vs_p53_freq$mean+0.5
 y_pos[21]=y_pos[21]-0.3
 y_pos[14]=y_pos[14]-0.3
 y_pos[29]=y_pos[29]-0.3
 y_pos[4]=y_pos[4]+0.3
-text(x = x_pos,y = y_pos,labels= average_num_smachete_fusions_vs_p53_freq$TCGA_Project, cex = 0.7,pos = 1)
+text(x = x_pos,y = y_pos,labels= average_num_DEEPEST_fusions_vs_p53_freq$TCGA_Project, cex = 0.7,pos = 1)
